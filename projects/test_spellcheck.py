fd23c500ba6f3a96569a408f70ffc1329d59bf84
baeliyn <ahmedberakurt0601@gmail.com>
2023-08-13 05:14:44 +0300

    fix

--- projects/test_spellcheck.py
+++ projects/test_spellcheck.py
@@ -1,19 +1,13 @@
-import pytest
 import spellcheck
 
 alpha = "Checking the length & structure of the sentence."
 
-@pytest.fixture
-def input_value():
-    input = alpha
-    return input
-
 def test_length(input_value):
 
-    assert spellcheck.word_count(input_value) < 10
-    assert spellcheck.char_count(input_value) < 50
+    assert spellcheck.word_count(alpha) < 10
+    assert spellcheck.char_count(alpha) < 50
 
 def test_struc(input_value):
 
-    assert spellcheck.first_char(input_value).isupper()
-    assert spellcheck.last_char(input_value) == "."
+    assert spellcheck.first_char(alpha).isupper()
+    assert spellcheck.last_char(alpha) == "."
